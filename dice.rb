a72cfe9795b54568432e210a97b9a013ff4a3ebe
feruncie <fruncie@chicagobooth.edu>
2024-07-08 19:35:02 +0000

    updates

--- dice.rb
+++ dice.rb
@@ -27,9 +27,8 @@ get ("/dice/2/6") do
   sum = first_die + second_die 
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  
+  erb(:two_six)
 end 
 
 get ("/dice/2/10") do
